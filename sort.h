acc5f3a59952567dd022b1dc4bff2df174cc91dc
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 18:01:25 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- sort.h
+++ sort.h
@@ -22,5 +22,7 @@ typedef struct listint_s
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void print_list(const listint_t *list);
+void selection_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 
 #endif
