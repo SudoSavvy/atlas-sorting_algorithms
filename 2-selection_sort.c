a5346f655a138b730228865a13da8c0734c8e1c2
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 18:03:43 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,25 +2,6 @@
 #include "sort.h"
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in the array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    for (i = 0; i < size; i++)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
-}
-
-/**
  * selection_sort - Sorts an array of integers in ascending order
  * using the Selection sort algorithm
  *
