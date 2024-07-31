a469690b63fe3c5eed9df7a6c892f1ea66bbbc09
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 17:56:48 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,16 +1,14 @@
 #include "sort.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                        using the Insertion sort algorithm.
- * @list: A pointer to the pointer to the head of the doubly linked list.
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm.
+ * @list: Double pointer to the head of the list.
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *key, *next;
-    int temp;
+    listint_t *current, *key, *temp;
 
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
@@ -20,42 +18,36 @@ void insertion_sort_list(listint_t **list)
     while (current != NULL)
     {
         key = current;
-        next = current->next;
+        current = current->next;
 
-        // Move the key to its correct position
         while (key->prev != NULL && key->n < key->prev->n)
         {
-            // Swap nodes
             swap_nodes(list, key->prev, key);
-            print_list(*list); // Print list after each swap
+            print_list(*list);  // Print the list after swapping
         }
-
-        current = next;
     }
 }
 
 /**
- * swap_nodes - Swaps two nodes in a doubly linked list
- * @list: A pointer to the pointer to the head of the doubly linked list.
- * @node1: The first node to swap.
- * @node2: The second node to swap.
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ * @list: Double pointer to the head of the list.
+ * @node1: Pointer to the first node.
+ * @node2: Pointer to the second node.
  */
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
     if (node1 == NULL || node2 == NULL || node1 == node2)
         return;
 
-    // Swap node1 and node2
-    if (node1->prev)
+    if (node1->prev != NULL)
         node1->prev->next = node2;
-    else
-        *list = node2;
-
-    if (node2->next)
+    if (node2->next != NULL)
         node2->next->prev = node1;
-
     node2->prev = node1->prev;
     node1->next = node2->next;
-    node2->next = node1;
     node1->prev = node2;
+    node2->next = node1;
+
+    if (*list == node1)
+        *list = node2;
 }
